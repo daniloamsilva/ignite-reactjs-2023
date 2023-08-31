f0fea2c160451134df5a0cb9ef6fcf83e2f20732
Danilo Augusto <danilo.augmonteiro@gmail.com>
2023-08-30 21:33:20 -0300

    Aula: Modal de nova transação

--- 03-conteudo-dt-money/src/pages/Transactions/components/SearchForm/styles.ts
+++ 03-conteudo-dt-money/src/pages/Transactions/components/SearchForm/styles.ts
@@ -21,6 +21,7 @@ export const SearchFormContainer = styled.form`
     display: flex;
     align-items: center;
     gap: 0.75rem;
+    cursor: pointer;
 
     padding: 1rem;
     background: transparent;
