8eeb757fc4d491a23b6f946b5327d5515e5a30a2
Danilo Augusto <danilo.augmonteiro@gmail.com>
2023-09-05 19:22:50 -0300

    Formatando valores

--- 03-conteudo-dt-money/src/pages/Transactions/index.tsx
+++ 03-conteudo-dt-money/src/pages/Transactions/index.tsx
@@ -5,6 +5,7 @@ import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 import { SearchForm } from "./components/SearchForm";
 import { TransactionContext } from "../../contexts/TransactionsContext";
+import { dateFormatter, priceFormatter } from "../../utils/formatter";
 
 export function Transactions() {
   const { transactions } = useContext(TransactionContext);
@@ -24,11 +25,12 @@ export function Transactions() {
                 <td width="50%">{transaction.description}</td>
                 <td>
                   <PriceHighlight variant={transaction.type}>
-                    {transaction.price}
+                    {transaction.type === 'outcome' && '- '}
+                    {priceFormatter.format(transaction.price)}
                   </PriceHighlight>
                 </td>
                 <td>{transaction.category}</td>
-                <td>{transaction.createdAt}</td>
+                <td>{dateFormatter.format(new Date(transaction.createdAt))}</td>
               </tr>
 
             ))}
